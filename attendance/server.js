59718cd1c25939e579e015bf2399633a675266c0
Masoud Moharrami <m.masoud.1991@gmail.com>
2018-06-04 20:12:09 +0430

    Attendance

---
+++ attendance/server.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var io = require('socket.io');
+var http = require('http');
+var bodyParser = require('body-parser');
+var cache = require('memory-cache');
+
+var app = express();
+var server = http.createServer(app);
+var listener = io.listen(server);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
+app.set('view options', {
+    layout: false
+});
+
+app.get('/student', function (req, res) {
+    res.render('student', {
+        title: 'Your Attendance Page',
+    });
+});
+
+app.get('/prof', function (req, res) {
+    res.render('prof', {
+        title: 'Student Attendance Page',
+    });
+});
+
+listener.on('connection', function (socket) {
+    
+    console.log('Connection to client established');
+    
+    socket.on('disconnect', function () {
+        console.log('Server has disconnected');
+    });
+    
+    
+    socket.on('chat message', function(data){
+
+        var name = data.name
+        var stIndex = name.indexOf("STUDENT")
+        var pIndex = name.indexOf("PROF")
+
+        if(stIndex > -1){
+            console.log(`student id: ${name.substring(7)}`)
+            listener.emit('chat message', {
+                name: data.name,
+                student: data.prof,
+                absence: data.absence
+            });
+        }else if(pIndex > -1){
+            console.log(`prof id: ${name.substring(4)}`)
+            listener.emit('chat message', {
+                name: data.name,
+                prof: data.prof,
+                absence: data.absence
+            });
+        }
+    });
+    
+});
+
+server.listen(8080);
