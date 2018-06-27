0644328a3597dd12f7366f427c807604dd76d9d1
Ali Shamakhi <ali.shamakhi.97@gmail.com>
2018-06-27 05:51:43 +0430

    api: check attendance assignment exists on attend get_course_session_timestamps() in util.php

--- api/util.php
+++ api/util.php
@@ -36,32 +36,74 @@ function get_weekday_from_day_name($day_name) {
         case 'mo':
         case 'mon':
         case 'monday':
-            return 1;
+        return 1;
         case 'tu':
         case 'tue':
         case 'tuesday':
-            return 2;
+        return 2;
         case 'we':
         case 'wed':
         case 'wednesday':
-            return 3;
+        return 3;
         case 'th':
         case 'thu':
         case 'thursday':
-            return 4;
+        return 4;
         case 'fr':
         case 'fri':
         case 'friday':
-            return 5;
+        return 5;
         case 'sa':
         case 'sat':
         case 'saturday':
-            return 6;
+        return 6;
         case 'su':
         case 'sun':
         case 'sunday':
-            return 7;
+        return 7;
         default:
             return 0;
     }
+}
+
+const MONDAY = 1;
+const TUESDAY = 2;
+const WEDNESDAY = 3;
+const THURSDAY = 4;
+const FRIDAY = 5;
+const SATURDAY = 6;
+const SUNDAY = 7;
+
+function get_course_session_timestamps($course_summary, int $current_timestamp) {
+    $session_start_timestamp = $current_timestamp - ($current_timestamp % 86400) + (7 * 60 * 60) - date('Z');
+    $session_end_timestamp = $current_timestamp - ($current_timestamp % 86400) + (19 * 60 * 60) - date('Z');
+    $current_weekday = date('N', $current_timestamp);
+    preg_match_all('/<p>(.*?)<\/p>/', $course_summary, $summary_paragraphs, PREG_PATTERN_ORDER);
+    $timetable_started = false;
+    foreach ($summary_paragraphs[1] as $summary_line) {
+        $line = strtolower(str_replace('&nbsp;', '', str_replace(' ', '', $summary_line)));
+        if ($line == '[timetable]') {
+            if ($timetable_started) {
+                break;
+            }
+            $timetable_started = true;
+            continue;
+        }
+        if ($line == '[/timetable]') {
+            break;
+        }
+        if ($timetable_started) {
+            preg_match_all('/([a-z]*)([0-9]{1,2}):([0-9]{2})-([0-9]{1,2}):([0-9]{2})/', $line, $time_parts, PREG_PATTERN_ORDER);
+            $weekday = get_weekday_from_day_name($time_parts[1][0]);
+            if ($weekday == $current_weekday) {
+                $session_start_timestamp = $current_timestamp - ($current_timestamp % 86400) + ((int)$time_parts[2][0]) * 60 * 60 + ((int)$time_parts[3][0]) * 60 - date('Z');
+                $session_end_timestamp = $current_timestamp - ($current_timestamp % 86400) + ((int)$time_parts[4][0]) * 60 * 60 + ((int)$time_parts[5][0]) * 60 - date('Z');
+                break;
+            }
+        }
+    }
+    $session_timestamps = new stdClass;
+    $session_timestamps->start = $session_start_timestamp;
+    $session_timestamps->end = $session_end_timestamp;
+    return $session_timestamps;
 }
